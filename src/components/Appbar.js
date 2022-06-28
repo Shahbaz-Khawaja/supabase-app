6babbfd7b2b95f10264b786b3e76611179f65f6d
Shahbaz Khawaja <shahbaz.khawaja@techno-soft.com>
2022-06-28 20:37:27 +0500

    template-react

---
+++ src/components/Appbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
+function Appbar() {
+  return (
+    <AppBar>
+      <Toolbar>
+        <Typography>Supabase App</Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Appbar;
